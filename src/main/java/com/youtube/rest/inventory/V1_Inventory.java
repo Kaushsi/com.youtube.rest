6e27013c6fd98c5007f58970a09e2027c2adafae
Kaushsi <shaily.kaushik@gmail.com>
2014-02-21 19:21:46 +1100

    intermitent changes

---
+++ src/main/java/com/youtube/rest/inventory/V1_Inventory.java
@@ -0,0 +1,45 @@
+package com.youtube.rest.inventory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import com.youtube.dao.OracleDao;
+import com.youtube.util.ToJSON;
+
+
+@Path("v1/inventory/*")
+public class V1_Inventory {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String returnAllPcParts()  throws Exception {
+		PreparedStatement query = null;
+		Connection conn = null;
+		String returnString = null;
+		
+		try {
+			conn = OracleDao.OracleDaoConn().getConnection();
+			query = conn.prepareStatement("select * from PC_PARTS");
+			ResultSet rs = query.executeQuery();
+			ToJSON converter = new ToJSON();
+			JSONArray jsonArray = converter.toJSONArray(rs);
+			query.close();
+	
+			returnString = jsonArray.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+}
---
+++ src/main/java/com/youtube/util/ToJSON.java
@@ -0,0 +1,92 @@
+package com.youtube.util;
+
+import java.sql.ResultSet;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+public class ToJSON {
+	
+	public JSONArray toJSONArray(ResultSet rs) {
+		JSONArray jsonArray = new JSONArray();
+		try {
+			java.sql.ResultSetMetaData rsmd = rs.getMetaData();
+			
+			while (rs.next()) {
+				int numColumns = rsmd.getColumnCount();
+				JSONObject obj = new JSONObject();
+				
+				for (int i = 1; i < numColumns + 1; i++) {
+					String columnName = rsmd.getColumnName(i);
+					if (rsmd.getColumnType(i)== java.sql.Types.ARRAY) {
+						obj.put(columnName, rs.getArray(columnName));
+						System.out.println("ToJSON: ARRAY");
+					} 
+					else if (rsmd.getColumnType(i)== java.sql.Types.BIGINT) {
+						obj.put(columnName, rs.getInt(columnName));
+						System.out.println("ToJSON: BIGINT");
+					} 
+					else if (rsmd.getColumnType(i)== java.sql.Types.BOOLEAN) {
+						obj.put(columnName, rs.getBoolean(columnName));
+						System.out.println("ToJSON: BOOLEAN");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.BLOB) {
+						obj.put(columnName, rs.getBlob(columnName));
+						System.out.println("ToJSON: BLOB");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.DOUBLE) {
+						obj.put(columnName, rs.getDouble(columnName));
+						System.out.println("ToJSON: DOUBLE");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.FLOAT) {
+						obj.put(columnName, rs.getFloat(columnName));
+						System.out.println("ToJSON: BOOLEAN");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.INTEGER) {
+						obj.put(columnName, rs.getInt(columnName));
+						System.out.println("ToJSON: INTEGER");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.VARCHAR) {
+						obj.put(columnName, rs.getString(columnName));
+						System.out.println("ToJSON: VARCHAR");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.NVARCHAR) {
+						obj.put(columnName, rs.getString(columnName));
+						System.out.println("ToJSON: NVARCHAR");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.DATE) {
+						obj.put(columnName, rs.getDate(columnName));
+						System.out.println("ToJSON: VARCHAR");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.TIMESTAMP) {
+						obj.put(columnName, rs.getTimestamp(columnName));
+						System.out.println("ToJSON: TIMESTAMP");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.NUMERIC) {
+						obj.put(columnName, rs.getBigDecimal(columnName));
+						System.out.println("ToJSON: NUMERIC");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.SMALLINT) {
+						obj.put(columnName, rs.getInt(columnName));
+						System.out.println("ToJSON: SMALLINT");
+					}
+					else if (rsmd.getColumnType(i)== java.sql.Types.TINYINT) {
+						obj.put(columnName, rs.getInt(columnName));
+						System.out.println("ToJSON: TINYINT	");
+					}
+					else {
+						obj.put(columnName, rs.getObject(columnName));
+						System.out.println("ToJSON: Object " + columnName);
+					}
+					jsonArray.put(obj);
+				}
+			}
+			
+		} catch (Exception e) {
+			
+		}
+		
+		return jsonArray;
+	}
+
+}
