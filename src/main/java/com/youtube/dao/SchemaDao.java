77871075ded919eb0d651f5063b16139c020f509
Kaushsi <shaily.kaushik@gmail.com>
2014-02-24 00:02:29 +1100

    commit6

--- src/main/java/com/youtube/dao/SchemaDao.java
+++ src/main/java/com/youtube/dao/SchemaDao.java
@@ -81,5 +81,49 @@ public class SchemaDao extends OracleDao {
 
 		return json;
 	}
+	
+	public int insertIntoPCParts(String TITLE, String CODE, String MAKER, String AVAIL, String DESCRIPTION) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation here
+			 * before starting to insert data into the database.
+			 * 
+			 * Important: The primary key on PC_PARTS table will auto increment.
+			 * 		That means the PC_PARTS_PK column does not need to be apart of the 
+			 * 		SQL insert query below.
+			 */
+			conn = getConnection();
+			query = conn.prepareStatement("insert into PC_PARTS " +
+					"(TITLE, CODE, MAKER, AVAIL, DESCRIPTION) " +
+					"VALUES ( ?, ?, ?, ?, ? ) ");
+
+			query.setString(1, TITLE);
+			query.setString(2, CODE);
+			query.setString(3, MAKER);
+
+			//PC_PARTS_AVAIL is a number column, so we need to convert the String into a integer
+			int avilInt = Integer.parseInt(AVAIL);
+			query.setInt(4, avilInt);
+
+			query.setString(5, DESCRIPTION);
+			query.executeUpdate(); //note the new command for insert statement
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			return 500; //if a error occurs, return a 500
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+
+		return 200;
+		
+		
+		
+	}
 
 }
--- src/main/java/com/youtube/rest/inventory/V2_Inventory.java
+++ src/main/java/com/youtube/rest/inventory/V2_Inventory.java
@@ -1,6 +1,8 @@
 package com.youtube.rest.inventory;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -8,6 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import com.youtube.dao.SchemaDao;
@@ -99,4 +102,47 @@ public class V2_Inventory {
 
 		return Response.ok(returnString).build();
 	}
+	
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) {
+		
+		String returnString = null;
+		JSONArray json = new JSONArray();
+		SchemaDao dao = new SchemaDao();
+		
+		try {
+			System.out.println("Incoming Data: " + incomingData);
+			
+			ObjectMapper mapper = new ObjectMapper();
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = dao.insertIntoPCParts(itemEntry.TITLE, itemEntry.CODE, itemEntry.MAKER, 
+												 itemEntry.AVAIL, itemEntry.DESCRIPTION);
+			if (http_code == 200) {
+//				returnString = json.toString();
+				returnString = "item inserted";
+			} else {
+				return Response.status(http_code).entity("Unable to process data").build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		return Response.ok(returnString).build();
+		
+	}
+}
+
+
+class ItemEntry {
+	
+	public String TITLE;
+	public String CODE;
+	public String MAKER;
+	public String AVAIL;
+	public String DESCRIPTION;
+	
 }
